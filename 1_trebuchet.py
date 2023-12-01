809a2715380c0fcdaf87c11321c7ded83a64ef8d
RSKriegs <kaczarg@gmail.com>
2023-12-01 22:08:28 +0100

    day 1 part 2

--- 1_trebuchet.py
+++ 1_trebuchet.py
@@ -1,10 +1,31 @@
 #day 1
+import re
 
-with open(" ", "r") as file:
-    items = file.read().replace('\n', ' ').split(" ") 
+with open("data/input_1.txt", "r") as file:
+    items = file.read().replace('\n', ' ').split(" ")
+
+numbers = {
+    "one": 'o1e',
+    "two": 't2o',
+    "three": 'th3ee',
+    "four": 'f4ur',
+    "five": 'f5ve',
+    "six": 's6x',
+    "seven": 'se7en',
+    "eight": 'ei8ht',
+    "nine": 'n9ne'
+}
 
 final_list = []
 for item in items:
+    temp = ''
+    for x in list(item):
+        i += 1
+        temp += x
+        for k, v in numbers.items():
+            if k in temp:
+                temp = temp.replace(k,v)
+        item = temp
     item = list(filter(lambda x: x.isdigit(), list(item)))
     if item:
         final_list.append(int(str(f'{item[0]}{item[-1]}')))
