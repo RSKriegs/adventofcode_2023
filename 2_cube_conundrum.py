eefeec6db76135b1226b451f25bc6c33b6d34c7f
RSKriegs <kaczarg@gmail.com>
2023-12-03 02:24:49 +0100

    2nd day 2nd part

--- 2_cube_conundrum.py
+++ 2_cube_conundrum.py
@@ -7,7 +7,8 @@ for item in items[:-1]: #skip the last empty one
     temp = item.replace(';',',').split(": ")
     list_of_dicts.append({temp[0] : temp[1].split(', ')})
 
-result = 0
+#1st question
+result_1 = 0
 n = 1
 for item_dict in list_of_dicts:
     to_add = True
@@ -19,6 +20,23 @@ for item_dict in list_of_dicts:
             to_add = False
             break
     if to_add:
-        result += n
+        result_1 += n
     n += 1
-print(result)
+print(result_1)
+
+#2nd question
+result_2 = 0
+n = 1
+for item_dict in list_of_dicts:
+    max_dict = {'green': None, 'red': None, 'blue': None}
+    for item in item_dict[f'Game {n}']:
+        item = item.split(' ')
+        if max_dict[item[1]] is None or int(item[0]) > max_dict[item[1]]:
+            max_dict[item[1]] = int(item[0])
+    power = 1
+    for value in max_dict.values():
+        power *= value
+    result_2 += power
+    n += 1
+
+print(result_2)
