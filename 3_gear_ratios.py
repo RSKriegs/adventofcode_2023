3ae06f3fd88fdc1369f38116fb9732ccc0984768
RSKriegs <kaczarg@gmail.com>
2023-12-03 14:41:15 +0100

    3 day part 1 done

--- 3_gear_ratios.py
+++ 3_gear_ratios.py
@@ -1,5 +1,5 @@
 #day 1
-with open("data/input_3.txt", "r") as file:
+with open("", "r") as file:
     items = file.read().rstrip().split('\n')
 
 #create an initial matrix from input
@@ -11,16 +11,16 @@ for i in range(0, len(items)):
 matrix_map = [[False for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
 for i in range(len(matrix)):
     for j in range(len(matrix[0])):
-        if matrix[i][j] in "!@#$%^&*()_-+={}[]":
+        if matrix[i][j] not in "0123456789.":
             for x in range(max(0, i - 1), min(i + 2, len(matrix))):
                     for y in range(max(0, j - 1), min(j + 2, len(matrix[0]))):
                         matrix_map[x][y] = True
 
 # retrieve elements - create a string to be converted to the integer 
 elements = []
-to_convert_to_int = ''
-to_append = False
 for i in range(len(matrix)):
+    to_convert_to_int = ''
+    to_append = False
     for j in range(len(matrix[0])):
         if matrix[i][j] in "0123456789":
             to_convert_to_int += str(int(matrix[i][j]))
