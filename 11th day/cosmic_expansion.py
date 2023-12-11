eb0274b4a9e6b17bbe362118d04690c08c652c1b
RSKriegs <kaczarg@gmail.com>
2023-12-11 21:54:43 +0100

    11th day part 2

--- 11th day/cosmic_expansion.py
+++ 11th day/cosmic_expansion.py
@@ -5,7 +5,6 @@ def transform_galaxies(cosmos):
         global counter
         if counter > 1:
             return cosmos
-        print(cosmos)
         final_cosmos = []
         cosmos = [list(x) for x in list(map(list, zip(*cosmos)))]
         for sublist in cosmos:
@@ -25,6 +24,36 @@ def transform_galaxies(cosmos):
                 galaxies.append((x,y))
     return galaxies
 
+def transform_galaxies_2(cosmos):
+    def expand_galaxies(cosmos):
+        global counter
+        if counter > 1:
+            return cosmos
+        final_cosmos = []
+        cosmos = [list(x) for x in list(map(list, zip(*cosmos)))]
+        for sublist in cosmos:
+            final_cosmos.append(sublist)
+            if '#' not in sublist:
+                final_cosmos.append(sublist)
+        counter += 1
+        return expand_galaxies(final_cosmos)
+
+    cosmos = [x for x in map(lambda x: list(x), cosmos)]
+    cosmos = expand_galaxies(cosmos)
+
+    galaxies = []
+    dummy_x = 0
+    for x in range(0, len(cosmos)):
+        dummy_y = 0
+        if '#' not in cosmos[x]:
+            dummy_x += 499999
+        for y in range(0, len(cosmos[x])):
+            if '#' not in [list(x) for x in list(map(list, zip(*cosmos)))][y]:
+                dummy_y += 499999
+            if cosmos[x][y] == "#":
+                galaxies.append((x + dummy_x,y + dummy_y))
+    return galaxies
+
 def manhattan_distance(point1, point2):
     return sum(abs(value1 - value2) for value1, value2 in zip(point1, point2))
 
@@ -32,6 +61,7 @@ if __name__ == "__main__":
     with open("data/input_11.txt", "r") as file:
         items = file.read().rstrip().split('\n')
     
+    #part 1
     counter = 0
     galaxies = transform_galaxies(items)
 
@@ -39,3 +69,12 @@ if __name__ == "__main__":
     s = sum([manhattan_distance(combo[0], combo[1]) for combo in combos])
 
     print(s)
+
+    #part 2
+    counter = 0
+    galaxies = transform_galaxies_2(items)
+
+    combos = list(combinations(galaxies, 2))
+    s = sum([manhattan_distance(combo[0], combo[1]) for combo in combos])
+    print(s)
+    
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 A repository for my debut year of 2023 in Advent of Code. Let's see how long I gonna last.
 
-Parts completed: 20/22 <br>
+Parts completed: 22/22 <br>
 1st day - 2 parts done <br>
 2nd day - 2 parts done <br>
 3rd day - 2 parts done <br>
@@ -11,4 +11,4 @@ Parts completed: 20/22 <br>
 8th day - 2 parts done <br>
 9th day - 2 parts done <br>
 10th day - 2 parts done <br>
-11th day - 1 part done <br>
+11th day - 2 parts done <br>
