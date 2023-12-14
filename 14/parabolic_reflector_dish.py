50275168974c11c9013abc9e4b68db116e0f9492
RSKriegs <kaczarg@gmail.com>
2023-12-14 22:05:53 +0100

    14th day part 2 preserve

--- 14/parabolic_reflector_dish.py
+++ 14/parabolic_reflector_dish.py
@@ -1,10 +1,8 @@
-if __name__=="__main__":
+import numpy as np
+from copy import deepcopy
 
-    with open("data/input_14.txt", "r") as file:
-        items = file.read().rstrip().split('\n')
-    
+def tilt_beams(items):
     items = list(map(list, zip(*items)))
-    
     for item in items:
         i = j = 0
         while i < len(item):
@@ -20,11 +18,22 @@ if __name__=="__main__":
                     break
             i += 1
             j += 1
+    return items
 
+def get_result(items):
     result = 0
     for item in items:
         for i in range(0, len(item)):
             if item[i] == 'O':
                 result += len(item) - i
-    print(result)
-        
+    return result
+
+if __name__=="__main__":
+
+    with open("data/test.txt", "r") as file:
+        items = file.read().rstrip().split('\n')
+    #part 1
+    beams = tilt_beams(items)
+    print(get_result(beams))
+
+    #part 2
