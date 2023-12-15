fa840d40fd9fbf7dfc4b24c68514113af4415c7d
RSKriegs <kaczarg@gmail.com>
2023-12-15 20:50:05 +0100

    15th day part 2 done

--- 15/lens_library.py
+++ 15/lens_library.py
@@ -1,14 +1,36 @@
+from collections import defaultdict
+
+def hash_item(item):
+    temp = 0
+    for string in item:
+        temp = (temp + ord(string)) * 17 % 256
+    return temp
+
+def calculate(items):
+    def assign_arguments(item): #label, length, operation
+        return (item[:-1], None, '-') if item[-1] == '-' else (item[:-2], int(item[-1]), '=')
+    items = map(assign_arguments, items)
+    boxes = defaultdict(dict)
+    for item in items:
+        hash = hash_item(item[0])
+        if item[2] == '-':
+            if item[0] in boxes[hash]:
+                del boxes[hash][item[0]]
+        elif item[2] == '=':
+            boxes[hash][item[0]] = item[1]
+    focusing_power = 0
+    for i, box in boxes.items():
+        for j, k in enumerate(box.values()):
+            focusing_power += (i + 1) * (j + 1) * k
+    return focusing_power
+
 if __name__=="__main__":
 
     with open("data/input_15.txt", "r") as file:
         items = file.read().rstrip().split(',')
     
-    result = 0 
-    for item in items:
-        temp = 0
-        for string in item:
-            temp = (temp + ord(string)) * 17 % 256
-        result += temp
-    
-    # Part 1
-    print(result)
+    #part 1
+    print(sum(hash_item(item) for item in items))
+
+    #part 2
+    print(calculate(items))
--- readme.md
+++ readme.md
@@ -1,8 +1,8 @@
 A repository for my debut year of 2023 in Advent of Code. Let's see how long I gonna last.
 
-My personal target: complete as many puzzles as I will be able to until 23th December (40 parts are a minimum goal).
+My goal: complete as many puzzles as I will be able to until 23th December (40 parts are my personal target).
 
-Parts completed: 29/30 <br>
+Parts completed: 30/30 <br>
 1st day - 2 parts done <br>
 2nd day - 2 parts done <br>
 3rd day - 2 parts done <br>
@@ -17,4 +17,4 @@ Parts completed: 29/30 <br>
 12th day - 2 parts done <br>
 13th day - 2 parts done <br>
 14th day - 2 parts done <br>
-15th day - 1 part done <br>
+15th day - 2 parts done <br>
